3eda637e48ff7be1d475b904160fa0b7e837b0b4
FaizaAbdullah-code <nedianfaiza2019@gmail.com>
2021-07-04 20:16:02 +0500

    initial-commit

--- AdminLTE-master/pages/tables/data.php
+++ AdminLTE-master/pages/tables/data.php
@@ -283,7 +283,7 @@ if(isset($_POST['submit'])){
                 <b>Version</b> 3.1.0
             </div>
             <strong>Copyright &copy; 2014-2021 <a href="">AGI Denim</a>.</strong> All rights
-            reserved..
+            reserved...
         </footer>
 
     </div>
