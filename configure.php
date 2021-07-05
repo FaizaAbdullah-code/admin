0507824047951fc7ad210bc1ed0cf58abc49cc75
FaizaAbdullah-code <74451890+FaizaAbdullah-code@users.noreply.github.com>
2021-07-06 02:34:45 +0500

    Add files via upload

---
+++ configure.php
@@ -0,0 +1,11 @@
+<?php 
+$con = mysqli_connect("localhost", "root", "");
+$db = mysqli_select_db($con, "hospital");
+// $con = mysqli_connect("remotemysql.com", "Em0CiQR5nW", "oIanirQhIk");
+// $db = mysqli_select_db($con, "Em0CiQR5nW");
+
+if (!$con) {
+ die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
