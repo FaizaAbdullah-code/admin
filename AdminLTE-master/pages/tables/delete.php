bc66fb18c06e02d8f8dcb778d77678329b83a08f
FaizaAbdullah-code <74451890+FaizaAbdullah-code@users.noreply.github.com>
2021-07-06 00:24:33 +0500

    Update delete.php

--- AdminLTE-master/pages/tables/delete.php
+++ AdminLTE-master/pages/tables/delete.php
@@ -9,7 +9,7 @@ $data=mysqli_query($con,$query);
 if($data){
    
     echo '<script type="text/javascript"> alert("Your entry Has Been Deleted") </script>';
-    header('Location: http://localhost:8080/admin/AdminLTE-master/pages/tables/data.php');
+    header('Location: https://industry-panel.herokuapp.com/AdminLTE-master/pages/tables/data.php');
 }
 else{
     echo "<script> alert('Sorry..!! Something Went Wrong') </script>";
