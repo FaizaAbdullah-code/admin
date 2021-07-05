9ff7213a1e17fb3a0a437efc6267977c0fc18a34
FaizaAbdullah-code <74451890+FaizaAbdullah-code@users.noreply.github.com>
2021-07-06 00:13:54 +0500

    Update configure.php

--- AdminLTE-master/pages/tables/configure.php
+++ AdminLTE-master/pages/tables/configure.php
@@ -1,9 +1,8 @@
 <?php 
-$con = mysqli_connect("localhost", "root", "");
-//Selecting Database
-$db = mysqli_select_db($con, "hospital");
-
-
+// $con = mysqli_connect("localhost", "root", "");
+// $db = mysqli_select_db($con, "hospital");
+$con = mysqli_connect("remotemysql.com", "Em0CiQR5nW", "oIanirQhIk");
+$db = mysqli_select_db($con, "Em0CiQR5nW");
 
 if (!$con) {
  die("Connection failed: " . mysqli_connect_error());
